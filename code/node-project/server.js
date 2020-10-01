dbae3d1937052bcb275e970838ee2500fd337670
AllanVergara <allanvergara95@hotmail.com>
2020-10-01 15:31:20 -0500

    Creacion de docente/network

--- code/node-project/server.js
+++ code/node-project/server.js
@@ -2,23 +2,12 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const response = require('./network/response')
 
-const router = express.Router()
+const router = require('./network/routes')
 
 var app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(router)
-
-router.get('/carrera', function(req, res) {
-    response.success(req, res, 'Lista de Carreras de la UPS', 200)
-})
-router.post('/carrera', function(req, res) {
-    if (req.query.error == 'ok') {
-        response.error(req, res, 'Error al ingresar la carrera.', 500)
-    } else {
-        response.success(req, res, 'Ingreso de Carrera exitoso.', 201)
-    }
-})
+router(app)
 
 app.use('/', express.static('public'))
 
